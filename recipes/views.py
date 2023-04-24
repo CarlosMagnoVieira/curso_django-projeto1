1d6b867e576c4e2bb2d755f1594a82abdd89bb13
Carlos Magno dos Santos Vieira <carlos.ufmt@yahoo.com.br>
2023-04-24 09:39:58 -0300

    Removido Contato e Sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home(request):
     return render(request,'recipes/home.html', context= {
         'nome': 'Carlos Magno'
     })
-
-def contato(request):
-    return render(request, 'me-apague/temp.html')
-
-def sobre(request):
-    return HttpResponse('sobre')
